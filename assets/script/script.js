a39d050a59666662d941b213c70b9bb946f3034f
Do Thien Hoang <theksbd@gmail.com>
2022-03-27 13:24:05 +0700

    Continue to building

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,16 @@
 // const password_length = document.getElementById("length").value;
-const lowercase = document.getElementById("lowercase");
-console.log(lowercase);
+// const lowercase = document.getElementById("lowercase");
+// console.log(lowercase);
 
-const generate = document.getElementById("submit").onclick = function() {
-    console.log("Hello");
+// const generate = document.getElementById("submit").onclick = function() {
+//     console.log("Hello");
+// }
+
+const password_res = 123456;
+function returnPass() {
+    document.getElementById("result").innerHTML = password_res;
+}
+
+function resetPass() {
+    document.getElementById("result").innerHTML = "Here";
 }
