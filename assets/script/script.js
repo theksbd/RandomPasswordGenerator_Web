0568cf4a1e3123d27d0083fb27dc9d0549250c2b
Do Thien Hoang <theksbd@gmail.com>
2022-05-23 14:24:01 +0700

    implement function random lower, uppercase and number

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,19 +6,57 @@
 //     console.log("Hello");
 // }
 
-let res = "";
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomCharacter = () => getRandomNumber(0, 3);
+
+function loopToGenerate(currentCharacter) {
+  // 0: lowercase, 1: uppercase, 2: number, 3: symbol
+  let value = "";
+  switch (currentCharacter) {
+    case 0:
+      value = getRandomNumber('a'.charCodeAt(0), 'z'.charCodeAt(0));
+      break;
+    case 1:
+      value = getRandomNumber('A'.charCodeAt(0), 'Z'.charCodeAt(0));
+      break;
+    case 2:
+      value = getRandomNumber('0'.charCodeAt(0), '9'.charCodeAt(0));
+      break;
+    default:
+      break;
+  }
+  return String.fromCharCode(value);
+}
 
 function generatePass() {
-  const length = document.getElementById("password-length").value;
+  let res = "";
+  const passwordLength = document.getElementById("password-length").value;
   const result = document.getElementById("result-text");
-  if (length <= 0)
+  if (passwordLength <= 0)
     result.innerText = "Your length is invalid to create a password";
   else {
     const containLowercase = document.querySelector("#lowercase").checked;
     const containUppercase = document.querySelector("#uppercase").checked;
     const containNumber = document.querySelector("#number").checked;
     const containSymbol = document.querySelector("#symbol").checked;
-      
+
+    const userSelection = [false, false, false, false];
+    if (containLowercase == true)
+      userSelection[0] = true;
+    if (containUppercase == true)
+      userSelection[1] = true;
+    if (containNumber == true)
+      userSelection[2] = true;
+    if (containSymbol == true)
+      userSelection[3] = true;
+
+    while (res.length < passwordLength) {
+      const currentCharacter = randomCharacter();
+      if (userSelection[currentCharacter] == false)
+        continue;
+      res += loopToGenerate(currentCharacter);
+    }
     result.innerText = `${res} is your password`;
   }
 }
